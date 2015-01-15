7694061c7d01cba49a2c203d7dda32fdec5b7dd5
wing2048 <atraguliauniliantauthr@gmail.com>
2015-01-15 18:16:52 +1100

    Added serial sending and Robot() object with functions

--- controller.py
+++ controller.py
@@ -9,16 +9,17 @@ class Robot():
         self.port = port
         self.serial = serial.Serial(port)
         self.send("import pyb")
-        # TODO Add pin numbers
-        self.send("yaw_servo = pyb.pin()")
-        self.send("bottom_pitch_servo = pyb.pin()")
-        self.send("middle_pitch_servo = pyb.pin()")
+        # TODO Add actual pin numbers -- you know, so it like works and stuff
+        self.send("yaw_servo = pyb.pin(Y1)")
+        self.send("bottom_pitch_servo = pyb.pin(Y2)")
+        self.send("middle_pitch_servo = pyb.pin(Y3)")
+        self.send("top_pitch_servo = pyb.pin(Y4)")
 
     def send(self, comm):
         self.serial.write((comm + '\r\t\n').encode())
 
     def left(self):
-        pass
+        self.send()
 
     def right(self):
         pass
@@ -29,17 +30,41 @@ class Robot():
     def down(self):
         pass
 
+    def test_led(self):
+        self.send('LED1.toggle()')
+
+    def update(self):
+        # TODO fix this
+        self.send('yaw_servo.angle(' + self.yaw_servo_angle + ')')
+        self.send('yaw_servo.angle(' + self.yaw_servo_angle + ')')
+        self.send('yaw_servo.angle(' + self.yaw_servo_angle + ')')
 
 robot = Robot('COM3')
 command = {
-    pygame.K_LEFT: robot.left(),
-    pygame.K_RIGHT: robot.right(),
-    pygame.K_UP: robot.up(),
-    pygame.K_DOWN: robot.down()
+    pygame.K_LEFT: robot.left,
+    pygame.K_RIGHT: robot.right,
+    pygame.K_UP: robot.up,
+    pygame.K_DOWN: robot.down,
+    pygame.K_SPACE: robot.test_led
 }
 pygame.init()
-
-while True:
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((100, 100))
+screen.fill((0, 0, 0))
+pygame.display.flip()
+done = False
+print('Ready for input')
+while not done:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            done = True
+        elif event == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                done = True
     pressed = pygame.key.get_pressed()
-    for key in pressed:
-        command[key]
+    for key in command:
+        if pressed[key]:
+            command[key]()
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
