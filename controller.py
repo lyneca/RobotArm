107bc145fabe5c98fea5e517f0af1625725a6c1d
wing2048 <atraguliauniliantauthr@gmail.com>
2015-01-24 09:44:28 +1100

    + hit v to print the servo angles + hit b to enter the name of a gesture, gestures stored in gestures.db + cleaned up

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 import serial
 import pygame
@@ -40,17 +41,53 @@ class Tool():
         self.is_on = False
 
 
+def tiny_wait():
+    time.sleep(0.1)
+
+
+def short_wait():
+    time.sleep(0.5)
+
+
+def wait():
+    time.sleep(1)
+
+
+def long_wait():
+    time.sleep(2)
+
+
 class Robot():
     def __init__(self, port, tool_pin):
         self.port = port
-        self.serial = serial.Serial(port)
-        self.send("import pyb")
-        self.send("tool_pin = pyb.Pin(" + tool_pin + ")")
-        self.send('tool_led = pyb.LED(2)')
-        self.send("yaw_servo = pyb.Servo(1)")
-        self.send("bottom_pitch_servo = pyb.Servo(2)")
-        self.send("middle_pitch_servo = pyb.Servo(3)")
-        self.send("top_pitch_servo = pyb.Servo(4)")
+        self.online = True
+        g_file = open('gestures.db')
+        ready = False
+        self.gestures = {}
+        name = False
+        for line in g_file:
+            if line[0] == ':':
+                name = ' '.join(line.split()[1:])
+                ready = True
+            elif line[0] == '$':
+                if ready:
+                    print(line.split()[1:])
+                    self.gestures[name] = ' '.join(line.split()[1:])
+                    ready = False
+        g_file.close()
+        print(self.gestures)
+        try:
+            self.serial = serial.Serial(port)
+            self.send("import pyb")
+            self.send("tool_pin = pyb.Pin(" + tool_pin + ")")
+            self.send('tool_led = pyb.LED(2)')
+            self.send("yaw_servo = pyb.Servo(1)")
+            self.send("bottom_pitch_servo = pyb.Servo(2)")
+            self.send("middle_pitch_servo = pyb.Servo(3)")
+            self.send("top_pitch_servo = pyb.Servo(4)")
+        except serial.SerialException:
+            print('ROBOT OFFLINE')
+            self.online = False
         self.servos = []
         self.yaw_servo = Servo(1)
         self.bottom_pitch_servo = Servo(2)
@@ -60,20 +97,54 @@ class Robot():
         self.tool = Tool()
         self.center()
         self.joints = []
+        self.command = {
+            'q': self.up,
+            'a': self.down,
+            'w': self.middle_up,
+            's': self.middle_down,
+            'e': self.top_up,
+            'd': self.top_down,
+            'z': self.left,
+            'x': self.right,
+            'Q': self.up,
+            'A': self.down,
+            'W': self.middle_up,
+            'S': self.middle_down,
+            'E': self.top_up,
+            'D': self.top_down,
+            'Z': self.left,
+            'X': self.right,
+            'c': self.center,
+            't': self.tool.on,
+            'T': self.tool.off,
+            'v': wait,
+            'V': long_wait,
+            'g': short_wait,
+            'G': tiny_wait,
+        }
+        self.top_joint = pygame.Rect((0, 0, 0, 0))
+        self.middle_joint = pygame.Rect((0, 0, 0, 0))
+        self.bottom_joint = pygame.Rect((0, 0, 0, 0))
+        self.yaw_joint = pygame.Rect((0, 0, 0, 0))
 
     def send(self, comm):
         self.serial.write((comm + '\r\t\n').encode())
+        self.flush()
+
+    def flush(self):
+        self.serial.flushInput()
+        self.serial.flushOutput()
+
+    def right(self, amount=move_amount):
+        self.yaw_servo.angle += amount
+        if self.yaw_servo.angle > 90:
+            self.yaw_servo.angle = 90
 
     def left(self, amount=move_amount):
         # self.send('yaw_servo.angle()')
         self.yaw_servo.angle -= amount
         if self.yaw_servo.angle < -90:
             self.yaw_servo.angle = -90
-
-    def right(self, amount=move_amount):
-        self.yaw_servo.angle += amount
-        if self.yaw_servo.angle > 90:
-            self.yaw_servo.angle = 90
 
     def up(self, amount=move_amount):
         self.bottom_pitch_servo.angle += amount
@@ -126,34 +197,53 @@ class Robot():
     def test_led(self):
         self.send('LED1.toggle()')
 
-    # noinspection PyAttributeOutsideInit,PyAttributeOutsideInit
+    # noinspection PyShadowingNames
+    def interpret(self, c_str, screen):
+        for letter in c_str:
+            if letter in 'QAWSEDZX':
+                self.command[letter](10)
+            elif letter in 'vVgG':
+                self.update(screen)
+                pygame.display.flip()
+                self.command[letter]()
+            else:
+                self.command[letter]()
+            self.update(screen)
+            pygame.display.flip()
+
+    def gesture_interpret(self, gesture):
+        gesture_lib = {
+            'y': self.yaw_servo.set_angle,
+            'b': self.bottom_pitch_servo.set_angle,
+            'm': self.middle_pitch_servo.set_angle,
+            't': self.top_pitch_servo.set_angle,
+        }
+        for g_set in gesture.split('+'):
+            for action in g_set.split():
+                gesture_lib[action[0]](float(action[1:]))
+            self.update(screen)
+            short_wait()
+
+    # noinspection PyShadowingNames
     def update(self, screen):
-        # OLD VELOCITY STUFF
-        # self.yaw_servo.velocity *= FRICTION_CONSTANT
-        # self.yaw_servo.angle += self.yaw_servo.velocity
-        # self.top_pitch_servo.velocity *= FRICTION_CONSTANT
-        # self.top_pitch_servo.angle += self.top_pitch_servo.velocity
-        # self.middle_pitch_servo.velocity *= FRICTION_CONSTANT
-        # self.middle_pitch_servo.angle += self.middle_pitch_servo.velocity
-        # self.bottom_pitch_servo.velocity *= FRICTION_CONSTANT
-        # self.bottom_pitch_servo.angle += self.bottom_pitch_servo.velocity
-        if self.tool.is_on:
-            self.send('tool_pin.high()')
-            self.send('tool_led.on()')
-        else:
-            self.send('tool_pin.low()')
-            self.send('tool_led.off()')
-        self.send('yaw_servo.angle(' + str(self.yaw_servo.angle) + ')')
-        self.send('yaw_servo.angle(' + str(self.bottom_pitch_servo.angle) + ')')
-        self.send('yaw_servo.angle(' + str(self.middle_pitch_servo.angle) + ')')
-        self.send('yaw_servo.angle(' + str(self.top_pitch_servo.angle) + ')')
+        if self.online:
+            if self.tool.is_on:
+                self.send('tool_pin.high()')
+                self.send('tool_led.on()')
+            else:
+                self.send('tool_pin.low()')
+                self.send('tool_led.off()')
+            self.send('yaw_servo.angle(' + str(self.yaw_servo.angle) + ')')
+            self.send('yaw_servo.angle(' + str(self.bottom_pitch_servo.angle) + ')')
+            self.send('yaw_servo.angle(' + str(self.middle_pitch_servo.angle) + ')')
+            self.send('yaw_servo.angle(' + str(self.top_pitch_servo.angle) + ')')
         screen.fill((255, 255, 255))
         # TODO: Fix ground boundaries
         yaw_line_end = (
             round(YAW_INDICATOR_CENTER[0] + 100 * math.cos(math.radians(
-                -1*(self.yaw_servo.angle + 90)))),
+                -1 * (self.yaw_servo.angle + 90)))),
             round(YAW_INDICATOR_CENTER[1] + 100 * math.sin(math.radians(
-                -1*(self.yaw_servo.angle + 90)))))
+                -1 * (self.yaw_servo.angle + 90)))))
         yaw_line_end = (yaw_line_end[0], YAW_INDICATOR_CENTER[1] * 2 - yaw_line_end[1])
         bp_line_end = (
             round(PITCH_INDICATOR_CENTER[0] + BOTTOM_ARM_LENGTH * math.cos(math.radians(
@@ -179,18 +269,32 @@ class Robot():
         pygame.draw.circle(screen, (0, 0, 0), PITCH_INDICATOR_CENTER, 3)
         pygame.draw.circle(screen, (0, 0, 0), bp_line_end, 3)
         pygame.draw.circle(screen, (0, 0, 0), mp_line_end, 3)
-        compensation = self.top_pitch_servo.angle + self.middle_pitch_servo.angle + self.bottom_pitch_servo.angle + self.tool_servo.angle - 90
+        compensation = \
+            self.top_pitch_servo.angle + \
+            self.middle_pitch_servo.angle + \
+            self.bottom_pitch_servo.angle + \
+            self.tool_servo.angle - 90
         if self.tool.is_on:
             pygame.draw.polygon(screen, (255, 255, 0), (tp_line_end,
-                                                        (tp_line_end[0]+30*math.cos(math.radians(LIGHT_ANGLE+compensation)), tp_line_end[1]+30*math.sin(math.radians(LIGHT_ANGLE+compensation))),
-                                                        (tp_line_end[0]+30*math.cos(math.radians(-LIGHT_ANGLE+compensation)), tp_line_end[1]+30*math.sin(math.radians(-LIGHT_ANGLE+compensation)))))
+                                                        (tp_line_end[0] + 30 * math.cos(
+                                                            math.radians(LIGHT_ANGLE + compensation)),
+                                                         tp_line_end[1] + 30 * math.sin(
+                                                             math.radians(LIGHT_ANGLE + compensation))),
+                                                        (tp_line_end[0] + 30 * math.cos(
+                                                            math.radians(-LIGHT_ANGLE + compensation)),
+                                                         tp_line_end[1] + 30 * math.sin(
+                                                             math.radians(-LIGHT_ANGLE + compensation)))))
             pygame.draw.rect(screen, (0, 255, 0), (screen.get_rect().width / 2 - 45, 205, 90, 40))
             pygame.draw.rect(screen, (0, 0, 0),
                              (tp_line_end[0] - 2, tp_line_end[1] - 2, 5, 5))
         else:
             pygame.draw.rect(screen, (255, 0, 0), (screen.get_rect().width / 2 - 45, 205, 90, 40))
             pygame.draw.circle(screen, (0, 0, 0), tp_line_end, 3)
+        pygame.draw.rect(screen, (0, 0, 0), (screen.get_rect().width / 2 + 100, 200, 100, 50), 5)
+        pygame.draw.rect(screen, (150, 150, 255), (screen.get_rect().width / 2 + 105, 205, 90, 40))
         tool_text = font.render('TOOL', 25, (0, 0, 0))
+        cntr_text = font.render('CNTR', 25, (0, 0, 0))
+        screen.blit(cntr_text, (screen.get_rect().centerx + 120, 260))
         pitch_text = font.render('ARM PITCH', 25, (0, 0, 0))
         yaw_text = font.render('YAW', 25, (0, 0, 0))
         screen.blit(pygame.transform.smoothscale(compass, (201, 110)),
@@ -220,6 +324,7 @@ class Robot():
             'top': self.top_joint,
             'yaw': self.yaw_joint
         }
+        pygame.display.flip()
 
 
 robot = Robot('COM3', 'Y1')
@@ -238,13 +343,13 @@ command = {
 }
 pygame.init()
 font = pygame.font.SysFont('Courier', 25)
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode((500, 300), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((500, 300))
 screen.fill((255, 255, 255))
 pygame.display.flip()
 done = False
 robot.update(screen)
 print('Ready for input')
+command_string = ''
 while not done:
     moving = False
     for event in pygame.event.get():
@@ -266,7 +371,28 @@ while not done:
             if sx <= mouse_pos[0] <= bx:
                 if sy <= mouse_pos[1] <= by:
                     robot.tool.toggle()
+            sx = screen.get_rect().width / 2 + 105
+            sy = 205
+            bx = sx + 90
+            by = sy + 40
+            if sx <= mouse_pos[0] <= bx:
+                if sy <= mouse_pos[1] <= by:
+                    robot.center()
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_b:
+                print('Enter gesture name:')
+                gesture = input(': ')
+                if gesture in robot.gestures:
+                    robot.gesture_interpret(robot.gestures[gesture])
+            if event.key == pygame.K_v:
+                print('Yaw:    ', robot.yaw_servo.angle)
+                print('Bottom: ', robot.bottom_pitch_servo.angle)
+                print('Middle: ', robot.middle_pitch_servo.angle)
+                print('Top:    ', robot.top_pitch_servo.angle)
+            if event.key == pygame.K_RETURN:
+                print('Enter command sequence string:')
+                command_string = input(': ')
+                robot.interpret(command_string, screen)
             if event.key == pygame.K_ESCAPE:
                 done = True
             if event.key == pygame.K_SPACE:
@@ -277,6 +403,7 @@ while not done:
                 'top': robot.middle_joint.center,
                 'middle': robot.bottom_joint.center,
                 'bottom': PITCH_INDICATOR_CENTER,
+
                 'yaw': YAW_INDICATOR_CENTER
             }
             mouse_pos = pygame.mouse.get_pos()
@@ -293,8 +420,10 @@ while not done:
             else:
                 n_angle = angle
             if active_joint == 'top':
-                if -90 + robot.bottom_pitch_servo.angle + robot.middle_pitch_servo.angle <= n_angle <= 90 + robot.bottom_pitch_servo.angle + robot.middle_pitch_servo.angle:
-                    robot.top_pitch_servo.set_angle(n_angle - robot.middle_pitch_servo.angle - robot.bottom_pitch_servo.angle)
+                if -90 + robot.bottom_pitch_servo.angle + robot.middle_pitch_servo.angle <= n_angle <= 90 + \
+                        robot.bottom_pitch_servo.angle + robot.middle_pitch_servo.angle:
+                    robot.top_pitch_servo.set_angle(
+                        n_angle - robot.middle_pitch_servo.angle - robot.bottom_pitch_servo.angle)
             elif active_joint == 'middle':
                 if -90 + robot.bottom_pitch_servo.angle <= n_angle <= 90 + robot.bottom_pitch_servo.angle:
                     robot.middle_pitch_servo.set_angle(n_angle - robot.bottom_pitch_servo.angle)
